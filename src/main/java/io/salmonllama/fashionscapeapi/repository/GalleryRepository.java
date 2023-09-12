b74bdad322bcef6971b84dd213ec9b46f78b32ea
Alex Gryczewski <alex.gryczewski1@gmail.com>
2023-09-12 11:15:21 -0400

    Standardize with identification variables

--- src/main/java/io/salmonllama/fashionscapeapi/repository/GalleryRepository.java
+++ src/main/java/io/salmonllama/fashionscapeapi/repository/GalleryRepository.java
@@ -9,9 +9,9 @@ import java.util.Optional;
 
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
 
-    @Query(value = "SELECT * FROM gallery g WHERE g.server_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT g FROM gallery g WHERE g.server_id = ?1", nativeQuery = true)
     public List<Gallery> findAllByServer(String serverId);
 
-    @Query(value = "SELECT * FROM gallery g WHERE g.server_id = ?1 and g.channel_id = ?2", nativeQuery = true)
+    @Query(value = "SELECT g FROM gallery g WHERE g.server_id = ?1 and g.channel_id = ?2", nativeQuery = true)
     public Optional<Gallery> findByServerAndChannel(String serverId, String channelId);
 }
