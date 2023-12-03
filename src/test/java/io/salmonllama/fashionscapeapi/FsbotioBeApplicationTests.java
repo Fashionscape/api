ad68aa8a334f16e0fa653142f0bbdbd65cda34c9
Salmonllama <agryczewski@outlook.com>
2023-12-03 07:43:07 -0500

    chore: Remove tests
    Tests are currently unused and were interfering with build statuses
    
    FS-30

--- src/test/java/io/salmonllama/fashionscapeapi/FsbotioBeApplicationTests.java
+++ src/test/java/io/salmonllama/fashionscapeapi/FsbotioBeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FsbotioBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
